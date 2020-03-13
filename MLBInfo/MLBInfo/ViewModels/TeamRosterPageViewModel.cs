2ffe87b129607a1ccdf4e05538e9d44139bcb90d
manuel41 <medmundogonzalez@gmail.com>
2020-03-12 21:43:36 -0400

    Create new views and viewmodels to be used later on

---
+++ MLBInfo/MLBInfo/ViewModels/TeamRosterPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLBInfo.ViewModels
+{
+    class TeamRosterPageViewModel
+    {
+    }
+}
---
+++ MLBInfo/MLBInfo/Views/TeamRosterPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MLBInfo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TeamRosterPage : ContentPage
+    {
+        public TeamRosterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
