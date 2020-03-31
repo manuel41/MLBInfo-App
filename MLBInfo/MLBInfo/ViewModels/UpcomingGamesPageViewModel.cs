eb35c9cadefb6f89a54f5ba19132689fa737b4bf
manuel41 <medmundogonzalez@gmail.com>
2020-03-30 22:55:00 -0400

    Add method to get upcoming games

--- MLBInfo/MLBInfo/ViewModels/UpcomingGamesPageViewModel.cs
+++ MLBInfo/MLBInfo/ViewModels/UpcomingGamesPageViewModel.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using MLBInfo.Models;
+using MLBPlayersApp.Services;
+using Prism.Navigation;
+using Prism.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MLBInfo.ViewModels
 {
-    class UpcomingGamesPageViewModel
+    public class UpcomingGamesPageViewModel : BaseViewModel, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Games> UpcomingGamesList { get; set; }
+
+        public UpcomingGamesPageViewModel(INavigationService navigationService, IApiService apiService, PageDialogService pagedialogservice) : base(navigationService, apiService, pagedialogservice)
+        {
+
+        }
+
+        public async Task GetFutureGames()
+        {
+            if(await this.HasInternet())
+            {
+                try
+                {
+                    GamesResults results = await ApiService.GetUpcomingGames();
+                    UpcomingGamesList = new ObservableCollection<Games>(results.GamesList as List<Games>);
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine($"API EXCEPTION {ex}");
+                }
+            }
+        }
     }
 }
