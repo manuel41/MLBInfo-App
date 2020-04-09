6b17766b1c718871d1be0edba51f8be231e93a14
manuel41 <medmundogonzalez@gmail.com>
2020-04-09 18:26:21 -0400

    Fix constants

--- MLBInfo/MLBInfo/Utilities/Config.cs
+++ MLBInfo/MLBInfo/Utilities/Config.cs
@@ -8,7 +8,7 @@ namespace MLBApp
     {
       public const string Url = "https://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code='mlb'&all_star_sw='N'";
       public const string Url2 = "https://lookup-service-prod.mlb.com/json/named";
-        public const string logos_url = "https://v1.baseball.api-sports.io/teams?country=USA";
-        public const string player_image = "https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=";
+        public const string Logos_Url = "https://v1.baseball.api-sports.io/teams?country=USA";
+        public const string Player_Image = "https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=";
     }
 }
